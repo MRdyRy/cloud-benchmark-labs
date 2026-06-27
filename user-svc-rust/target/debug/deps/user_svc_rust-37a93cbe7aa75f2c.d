3e85b4eebd988b6c473a62bb00d10fdb1a0d652c
Rudy Ryanto <rudyryanto@MacBook-Pro.local>
2026-06-28 01:37:09 +0700

    move java

---
+++ user-svc-rust/target/debug/deps/user_svc_rust-37a93cbe7aa75f2c.d
@@ -0,0 +1,5 @@
+/Users/rudyryanto/workspace/ngoprek/cloud-benchmark-labs/user-svc-rust/target/debug/deps/user_svc_rust-37a93cbe7aa75f2c.d: src/main.rs
+
+/Users/rudyryanto/workspace/ngoprek/cloud-benchmark-labs/user-svc-rust/target/debug/deps/libuser_svc_rust-37a93cbe7aa75f2c.rmeta: src/main.rs
+
+src/main.rs:
---
+++ user-svc-rust/target/debug/deps/user_svc_rust-610cf03f5160415b.d
@@ -0,0 +1,5 @@
+/Users/rudyryanto/workspace/ngoprek/cloud-benchmark-labs/user-svc-rust/target/debug/deps/user_svc_rust-610cf03f5160415b.d: src/main.rs
+
+/Users/rudyryanto/workspace/ngoprek/cloud-benchmark-labs/user-svc-rust/target/debug/deps/libuser_svc_rust-610cf03f5160415b.rmeta: src/main.rs
+
+src/main.rs:
